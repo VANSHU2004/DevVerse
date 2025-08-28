2a652d359463bcee053dcb3882f1fd9841946655
Atul Sharma <atulsharma02004@gmail.com>
2025-08-28 16:47:13 +0530

    Key

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
 function connect(){
-    mongoose.connect(process.env.MONGO_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("Connected to MongoDB")
     })
     .catch(err => {
