32a0fd8813e1241b38094faf69a72bfe2f1e8e87
Atul Sharma <atulsharma02004@gmail.com>
2025-08-28 16:36:05 +0530

    db update

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 function connect(){
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=>{
         console.log("Connected to MongoDB")
     })
     .catch(err => {
